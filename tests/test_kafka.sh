972a27964c5c202b70124fa86651ff245333e978
Hari Sekhon <harisekhon@gmail.com>
2016-06-08 17:12:02 +0100

    updated test_kafka.sh

--- tests/test_kafka.sh
+++ tests/test_kafka.sh
@@ -31,6 +31,8 @@ echo "
 # TODO: latest container 2.11_0.10 doesn't work yet, no leader takes hold
 #export KAFKA_VERSIONS="2.11_0.10 2.11_0.10 latest"
 export KAFKA_VERSIONS="${@:-latest 2.10_0.8 2.11_0.8 2.10_0.9 2.11_0.9}"
+# TODO: remove this
+export KAFKA_VERSIONS="${@:-2.10_0.9}"
 
 KAFKA_HOST="${DOCKER_HOST:-${KAFKA_HOST:-${HOST:-localhost}}}"
 KAFKA_HOST="${KAFKA_HOST##*/}"
@@ -59,7 +61,8 @@ test_kafka(){
     docker exec -ti "$DOCKER_CONTAINER" kafka-topics.sh --zookeeper localhost:2181 --create --replication-factor 1 --partitions 1 --topic "$KAFKA_TOPIC" || :
 
     hr
-    scala target/scala-*/check_kafka-assembly-*.jar com.linkedin.harisekhon.CheckKafka -H $HOST -P $KAFKA_PORT
+    # 'scala' command not found on Travis CI
+    java -jar target/scala-*/check_kafka-assembly-*.jar com.linkedin.harisekhon.CheckKafka -H $HOST -P $KAFKA_PORT
     hr
     delete_container
     echo
