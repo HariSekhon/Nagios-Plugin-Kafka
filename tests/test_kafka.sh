43b8230e8dc995e87b930945cac0ac5b49a0d3c8
Hari Sekhon <harisekhon@gmail.com>
2016-06-08 14:15:38 +0100

    updated test_kafka.sh

--- tests/test_kafka.sh
+++ tests/test_kafka.sh
@@ -28,6 +28,10 @@ echo "
 # ============================================================================ #
 "
 
+# TODO: latest container 2.11_0.10 doesn't work yet, no leader takes hold
+#export KAFKA_VERSIONS="2.11_0.10 2.11_0.10 latest"
+export KAFKA_VERSIONS="${@:-latest 2.10_0.8 2.11_0.8 2.10_0.9 2.11_0.9}"
+
 KAFKA_HOST="${DOCKER_HOST:-${KAFKA_HOST:-${HOST:-localhost}}}"
 KAFKA_HOST="${KAFKA_HOST##*/}"
 KAFKA_HOST="${KAFKA_HOST%%:*}"
@@ -35,10 +39,6 @@ export KAFKA_HOST
 
 export ZOOKEEPER_PORT="${ZOOKEEPER_PORT:-2181}"
 export KAFKA_PORT="${KAFKA_PORT:-9092}"
-
-# TODO: latest container 2.11_0.10 doesn't work yet, no leader takes hold
-#export KAFKA_VERSIONS="2.11_0.10 2.11_0.10 latest"
-export KAFKA_VERSIONS="${@:-latest 2.10_0.8 2.11_0.8 2.10_0.9 2.11_0.9}"
 
 export DOCKER_IMAGE="harisekhon/kafka"
 export DOCKER_CONTAINER="nagios-plugin-kafka-test"
