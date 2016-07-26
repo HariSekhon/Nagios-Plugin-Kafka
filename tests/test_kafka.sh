bb37755b113692b060d9d23a6a94b8659fab4093
Hari Sekhon <harisekhon@gmail.com>
2016-07-27 00:01:42 +0100

    updated tests/test_kafka.sh

--- tests/test_kafka.sh
+++ tests/test_kafka.sh
@@ -69,9 +69,7 @@ test_kafka(){
     fi
     hr
     # 'scala' command not found on Travis CI
-    #java -jar target/scala-*/check_kafka-assembly-*.jar -H $HOST -P $KAFKA_PORT
-    #./check_kafka -H $KAFKA_HOST:$KAFKA_PORT -T "$KAFKA_TOPIC"
-    ./check_kafka $KAFKA_HOST:$KAFKA_PORT "$KAFKA_TOPIC"
+    ./check_kafka -B $KAFKA_HOST:$KAFKA_PORT -T "$KAFKA_TOPIC"
     hr
     delete_container
     echo
