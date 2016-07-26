76c4ceff6369e8f8a1160635c9c3d39469fd1f4b
Hari Sekhon <harisekhon@gmail.com>
2016-07-27 00:28:25 +0100

    renamed variables

--- src/main/scala/CheckKafka.scala
+++ src/main/scala/CheckKafka.scala
@@ -43,20 +43,20 @@ class CheckKafka extends CLI {
     var brokers: String = ""
     var topic: String = ""
     var partition: Int = 0
-    var last_offset: Long = 0
-    var jaas_config: Option[String] = None
-    val consumer_props = new Properties
-    val producer_props = new Properties
-    if (consumer_props eq producer_props) {
+    var lastOffset: Long = 0
+    var jaasConfig: Option[String] = None
+    val consumerProps = new Properties
+    val producerProps = new Properties
+    if (consumerProps eq producerProps) {
         throw new IllegalArgumentException("Consumer + Producer props should not be the same object")
     }
-    val producer_properties: InputStream = getClass.getResourceAsStream("/producer.properties")
-    if (producer_properties == null) {
+    val producerProperties: InputStream = getClass.getResourceAsStream("/producer.properties")
+    if (producerProperties == null) {
         log.error("could not find producer.properties file")
         System.exit(2)
     }
-    val consumer_properties: InputStream = getClass.getResourceAsStream("/consumer.properties")
-    if (consumer_properties == null) {
+    val consumerProperties: InputStream = getClass.getResourceAsStream("/consumer.properties")
+    if (consumerProperties == null) {
         log.error("could not find consumer.properties file")
         System.exit(2)
     }
@@ -101,60 +101,60 @@ class CheckKafka extends CLI {
 //                 }
 //                 0
 //            }
-        load_props()
-        setup_jaas()
+        loadProps()
+        setupJaas()
     }
 
-    def load_props(): Unit = {
-        consumer_props.put("bootstrap.servers", brokers)
-        producer_props.put("bootstrap.servers", brokers)
+    def loadProps(): Unit = {
+        consumerProps.put("bootstrap.servers", brokers)
+        producerProps.put("bootstrap.servers", brokers)
 
-        val consumer_props_args = consumer_props.clone().asInstanceOf[Properties]
-        consumer_props.load(consumer_properties)
+        val consumerPropsArgs = consumerProps.clone().asInstanceOf[Properties]
+        consumerProps.load(consumerProperties)
         if (log.isDebugEnabled) {
             log.debug("Loaded Consumer Properties from resource file:")
-            consumer_props.foreach({ case (k, v) => log.debug(s"  $k = $v") })
+            consumerProps.foreach({ case (k, v) => log.debug(s"  $k = $v") })
             log.debug("Loading Consumer Property args:")
-            consumer_props_args.foreach({ case (k, v) => log.debug(s"  $k = $v") })
+            consumerPropsArgs.foreach({ case (k, v) => log.debug(s"  $k = $v") })
         }
-        val consumer_in = new PipedInputStream
-        val consumer_out = new PipedOutputStream(consumer_in)
+        val consumerIn = new PipedInputStream
+        val consumerOut = new PipedOutputStream(consumerIn)
         new Thread(
             new Runnable() {
                 def run(): Unit = {
-                    consumer_props_args.store(consumer_out, "")
-                    consumer_out.close()
+                    consumerPropsArgs.store(consumerOut, "")
+                    consumerOut.close()
                 }
             }
         ).start()
-        consumer_props.load(consumer_in)
+        consumerProps.load(consumerIn)
         // enforce unique group to make sure we are guaranteed to received our unique message back
-        val group_id: String = s"$uuid, $date"
-        log.info(s"group id='$group_id'")
-        consumer_props.put("group.id", group_id)
+        val groupId: String = s"$uuid, $date"
+        log.info(s"group id='$groupId'")
+        consumerProps.put("group.id", groupId)
 
-        val producer_props_args = producer_props.clone().asInstanceOf[Properties]
-        producer_props.load(producer_properties)
+        val producerPropsArgs = producerProps.clone().asInstanceOf[Properties]
+        producerProps.load(producerProperties)
         if (log.isDebugEnabled) {
             log.debug("Loaded Producer Properties from resource file:")
-            producer_props.foreach({ case (k, v) => log.debug(s"  $k = $v") })
+            producerProps.foreach({ case (k, v) => log.debug(s"  $k = $v") })
             log.debug("Loading Producer Property args:")
-            producer_props_args.foreach({ case (k, v) => log.debug(s"  $k = $v") })
+            producerPropsArgs.foreach({ case (k, v) => log.debug(s"  $k = $v") })
         }
-        val producer_in = new PipedInputStream()
-        val producer_out = new PipedOutputStream(producer_in)
+        val producerIn = new PipedInputStream()
+        val producerOut = new PipedOutputStream(producerIn)
         new Thread(
             new Runnable() {
                 def run(): Unit = {
-                    producer_props_args.store(producer_out, "")
-                    producer_out.close()
+                    producerPropsArgs.store(producerOut, "")
+                    producerOut.close()
                 }
             }
         ).start()
-        producer_props.load(producer_in)
+        producerProps.load(producerIn)
     }
 
-    def setup_jaas(): Unit = {
+    def setupJaas(): Unit = {
         log.debug("setting up JAAS for Kerberos security")
         val DEFAULT_JAAS_FILE = "kafka_cli_jaas.conf"
         val HDP_JAAS_PATH = "/usr/hdp/current/kafka-broker/config/kafka_client_jaas.conf"
@@ -166,37 +166,37 @@ class CheckKafka extends CLI {
         } else {
             srcpath
         }
-        val jaas_default_config = Paths.get(jar.getParentFile.getAbsolutePath, "conf", DEFAULT_JAAS_FILE).toString
-        val jaas_prop = System.getProperty("java.security.auth.login.config")
-        if (jaas_config.nonEmpty && jaas_config.get.toString.nonEmpty) {
-            log.info(s"using JAAS config file arg '$jaas_config'")
-        } else if (jaas_prop != null) {
-            val jaas_file = new File(jaas_prop)
-            if (jaas_file.exists() && jaas_file.isFile()) {
-                jaas_config = Option(jaas_prop)
-                log.info(s"using JAAS config file from System property java.security.auth.login.config = '$jaas_config'")
+        val jaasDefaultConfig = Paths.get(jar.getParentFile.getAbsolutePath, "conf", DEFAULT_JAAS_FILE).toString
+        val jaasProp = System.getProperty("java.security.auth.login.config")
+        if (jaasConfig.nonEmpty && jaasConfig.get.toString.nonEmpty) {
+            log.info(s"using JAAS config file arg '$jaasConfig'")
+        } else if (jaasProp != null) {
+            val jaasFile = new File(jaasProp)
+            if (jaasFile.exists() && jaasFile.isFile()) {
+                jaasConfig = Option(jaasProp)
+                log.info(s"using JAAS config file from System property java.security.auth.login.config = '$jaasConfig'")
             } else {
-                log.warn(s"JAAS path specified in System property java.security.auth.login.config = '$jaas_prop' does not exist!")
+                log.warn(s"JAAS path specified in System property java.security.auth.login.config = '$jaasProp' does not exist!")
             }
         }
-        if (jaas_config.isEmpty || jaas_config.get.toString.isEmpty) {
-            val hdp_jaas_file = new File(HDP_JAAS_PATH)
-            if (hdp_jaas_file.exists() && hdp_jaas_file.isFile()) {
+        if (jaasConfig.isEmpty || jaasConfig.get.toString.isEmpty) {
+            val hdpJaasFile = new File(HDP_JAAS_PATH)
+            if (hdpJaasFile.exists() && hdpJaasFile.isFile()) {
                 log.info(s"found HDP Kafka kerberos config '$HDP_JAAS_PATH'")
-                jaas_config = Option(HDP_JAAS_PATH)
+                jaasConfig = Option(HDP_JAAS_PATH)
             }
         }
-        if (jaas_config.isEmpty || jaas_config.get.toString.isEmpty) {
-            val jaas_default_file = new File(jaas_default_config)
-            if (jaas_default_file.exists() && jaas_default_file.isFile()) {
-                log.info(s"using default JaaS config file '$jaas_default_config'")
-                jaas_config = Option(jaas_default_config)
+        if (jaasConfig.isEmpty || jaasConfig.get.toString.isEmpty) {
+            val jaasDefaultFile = new File(jaasDefaultConfig)
+            if (jaasDefaultFile.exists() && jaasDefaultFile.isFile()) {
+                log.info(s"using default JaaS config file '$jaasDefaultConfig'")
+                jaasConfig = Option(jaasDefaultConfig)
             } else {
                 log.warn("cannot find default JAAS file and none supplied")
             }
         }
-        if (jaas_config.nonEmpty && jaas_config.get.toString.nonEmpty) {
-            System.setProperty("java.security.auth.login.config", jaas_config.get)
+        if (jaasConfig.nonEmpty && jaasConfig.get.toString.nonEmpty) {
+            System.setProperty("java.security.auth.login.config", jaasConfig.get)
         } else {
             log.warn("no JAAS config defined")
         }
@@ -208,7 +208,7 @@ class CheckKafka extends CLI {
             // Exception in thread "main" org.apache.kafka.common.KafkaException: Failed to construct kafka consumer
             // ...
             // org.apache.kafka.common.config.ConfigException: Invalid url in bootstrap.servers: 192.168.99.100
-            run_test()
+            runTest()
         } catch {
             case e: KafkaException => {
                 println("Caught Kafka Exception: ")
@@ -223,47 +223,49 @@ class CheckKafka extends CLI {
         }
     }
 
-    def run_test(): Unit = {
-        log.debug("run_test()")
-        val start_time = System.currentTimeMillis()
+    def runTest(): Unit = {
+        log.debug("runTest()")
+        val startTime = System.currentTimeMillis()
         // Cannot use 0.8 consumers as only new 0.9 API supports Kerberos
         log.info("creating Kafka consumer")
-        val consumer = new KafkaConsumer[String, String](consumer_props)
+        val consumer = new KafkaConsumer[String, String](consumerProps)
         log.info("creating Kafka producer")
-        val producer = new KafkaProducer[String, String](producer_props)
+        val producer = new KafkaProducer[String, String](producerProps)
         subscribe(consumer=consumer, topic=topic, partition=partition)
-        val start_write = System.currentTimeMillis()
+        val startWrite = System.currentTimeMillis()
         produce(producer=producer, topic=topic, partition=partition, msg=msg)
-        val write_time = (System.currentTimeMillis() - start_write) / 1000.0
-        val read_start_time = System.currentTimeMillis()
+        val writeTime = (System.currentTimeMillis() - startWrite) / 1000.0
+        val readStartTime = System.currentTimeMillis()
         consume(consumer=consumer, topic=topic, partition=partition)
-        val end_time = System.currentTimeMillis()
-        val read_time = (end_time - read_start_time) / 1000.0
-        val total_time = (end_time - start_time) / 1000.0
+        val endTime = System.currentTimeMillis()
+        val readTime = (endTime - readStartTime) / 1000.0
+        val totalTime = (endTime - startTime) / 1000.0
         val plural =
-            if (consumer_props.get("bootstrap.servers").isInstanceOf[String] &&
-                consumer_props.get("bootstrap.servers").asInstanceOf[String].split("\\s+,\\s+").length > 1)
+            if (consumerProps.get("bootstrap.servers").isInstanceOf[String] &&
+                consumerProps.get("bootstrap.servers").asInstanceOf[String].split("\\s+,\\s+").length > 1)
             {
                 "s"
             } else {
                 ""
             }
-        val output = s"OK: Kafka broker$plural successfully returned unique message, write_time=${write_time}s, read_time=${read_time}s, total_time=${total_time}s | write_time=${write_time}s read_time=${read_time}s total_time=${total_time}s"
+        val output = s"OK: Kafka broker$plural successfully returned unique message" +
+                     s", write time = ${writeTime}s, read time = ${readTime}s, total time = ${totalTime}s " +
+                     s"| write_time=${writeTime}s read_time=${readTime}s total_time=${totalTime}s"
 //        log.info(output)
         println(output)
     }
 
     def subscribe(consumer: KafkaConsumer[String, String], topic: String = topic, partition: Int = partition): Unit = {
         log.debug(s"subscribe(consumer, $topic, $partition)")
-        val topic_partition = new TopicPartition(topic, partition)
+        val topicPartition = new TopicPartition(topic, partition)
         // conflicts with partition assignment
         // log.debug(s"subscribing to topic $topic")
         // consumer.subscribe(Arrays.asList(topic))
         log.info(s"consumer assigning topic '$topic' partition '$partition'")
-        consumer.assign(Arrays.asList(topic_partition))
+        consumer.assign(Arrays.asList(topicPartition))
         // consumer.assign(Arrays.asList(partition))
         // not connected to port so no conn refused at this point
-        last_offset = consumer.position(topic_partition)
+        lastOffset = consumer.position(topicPartition)
     }
 
     def produce(producer: KafkaProducer[String, String], topic: String = topic, partition: Int = partition, msg: String = msg): Unit = {
@@ -278,16 +280,16 @@ class CheckKafka extends CLI {
 
     def consume(consumer: KafkaConsumer[String, String], topic: String = topic, partition: Int = partition): Unit = {
         log.debug(s"consumer(consumer, $topic, $partition")
-        val topic_partition = new TopicPartition(topic, partition)
-        log.info(s"seeking to last known offset $last_offset")
-        consumer.seek(topic_partition, last_offset)
-        log.info(s"consuming from offset $last_offset")
+        val topicPpartition = new TopicPartition(topic, partition)
+        log.info(s"seeking to last known offset $lastOffset")
+        consumer.seek(topicPpartition, lastOffset)
+        log.info(s"consuming from offset $lastOffset")
         val records: ConsumerRecords[String, String] = consumer.poll(200) // ms
         log.info("closing consumer")
         consumer.close()
-        val consumed_record_count: Int = records.count()
-        log.info(s"consumed record count = $consumed_record_count")
-        assert(consumed_record_count != 0)
+        val consumedRecordCount: Int = records.count()
+        log.info(s"consumed record count = $consumedRecordCount")
+        assert(consumedRecordCount != 0)
         var msg2: String = null
         for (record: ConsumerRecord[String, String] <- records) {
             val record_topic = record.topic()
