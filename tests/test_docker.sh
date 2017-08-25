f719a489c7ce324ab23acaec9ce2164651cc7e27
Hari Sekhon <harisekhon@gmail.com>
2017-08-25 14:23:08 +0200

    added tests/test_docker.sh

---
+++ tests/test_docker.sh
@@ -0,0 +1,35 @@
+#!/usr/bin/env bash
+#  vim:ts=4:sts=4:sw=4:et
+#
+#  Author: Hari Sekhon
+#  Date: 2016-12-08 14:38:37 +0000 (Thu, 08 Dec 2016)
+#
+#  https://github.com/harisekhon/nagios-plugin-kafka
+#
+#  License: see accompanying Hari Sekhon LICENSE file
+#
+#  If you're using my code you're welcome to connect with me on LinkedIn and optionally send me feedback to help steer this or other code I publish
+#
+#  https://www.linkedin.com/in/harisekhon
+#
+
+set -euo pipefail
+DEBUG="${DEBUG:-}"
+[ -n "${DEBUG:-}" ] && set -x
+srcdir="$(cd "$(dirname "$0")" && pwd)"
+
+cd "$srcdir/.."
+
+. "bash-tools/docker.sh"
+. "bash-tools/utils.sh"
+
+section "Docker Image"
+
+export DOCKER_IMAGE="harisekhon/nagios-plugin-kafka"
+
+if is_CI && is_docker_available; then
+    [ -n "${NO_PULL:-}" ] ||
+        docker pull "$DOCKER_IMAGE"
+    set +e
+    docker run --rm -e "DEBUG=$DEBUG" -e "NO_GIT=1" -e TRAVIS="${TRAVIS:-}" "$DOCKER_IMAGE" tests/help.sh
+fi
